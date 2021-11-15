4f37c6b6203233a851a56e507b906e4172f70ebf
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-15 15:43:23 +0100

    basic layout for msg done

--- public/js/main.js
+++ public/js/main.js
@@ -6,52 +6,91 @@ const myApp = new Vue({
             avatar: './public/resources/img/avatar_io.jpg',
             visible: true
         },
-        contacts: [ 
-            { 
-                name: 'Michele', 
-                avatar: './public/resources/img/avatar_1.jpg', 
-                visible: false,
-                messages: [ 
-                    { 
-                        date: '10/01/2020 15:30:55', 
-                        text: 'Hai portato a spasso il cane?', 
-                        status: 'sent'
-                    }, 
-                    { 
-                        date: '10/01/2020 15:50:00', 
-                        text: 'Ricordati di dargli da mangiare', 
-                        status: 'sent'
-                    }, 
-                    { 
-                        date: '10/01/2020 16:15:22', 
-                        text: 'Tutto fatto!', 
-                        status: 'received'
-                    }
-                ], 
+        contacts: [
+            {
+               "name": "Michele",
+               "avatar": "./public/resources/img/avatar_1.jpg",
+               "visible": true,
+               "messages": [
+                  {
+                     "date": "10/01/2020 15: 30: 55",
+                     "text": "Hai portato a spasso il cane?",
+                     "status": "sent"
+                  },
+                  {
+                     "date": "10/01/2020 15: 50: 00",
+                     "text": "Ricordati di dargli da mangiare",
+                     "status": "sent"
+                  },
+                  {
+                     "date": "10/01/2020 16: 15: 22",
+                     "text": "Tutto fatto!",
+                     "status": "received"
+                  }
+               ]
             },
-            {   
-                name:   'Fabio',   
-                avatar:   './public/resources/img/avatar_2.jpg',   
-                visible:   false,   
-                messages:   [   
-                    {   
-                        date:   '20/03/2020   16:30:00',   
-                        text:   'Ciao   come   stai?',   
-                        status:   'sent'   
-                    },   
-                    {   
-                        date:   '20/03/2020   16:30:55',   
-                        text:   'Bene   grazie!   Stasera   ci   vediamo?',   
-                        status:   'received'   
-                    },   
-                    {   
-                        date:   '20/03/2020   16:35:00',   
-                        text:   'Mi   piacerebbe   ma   devo   andare   a   fare   la   spesa.',   
-                        status:   'sent'   
-                    }   
-                ],   
+            {
+               "name": "Fabio",
+               "avatar": "./public/resources/img/avatar_2.jpg",
+               "visible": false,
+               "messages": [
+                  {
+                     "date": "20/03/2020 16: 30: 00",
+                     "text": "Ciao come stai?",
+                     "status": "sent"
+                  },
+                  {
+                     "date": "20/03/2020 16: 30: 55",
+                     "text": "Bene grazie! Stasera ci vediamo?",
+                     "status": "received"
+                  },
+                  {
+                     "date": "20/03/2020 16: 35: 00",
+                     "text": "Mi piacerebbe ma devo andare a fare la spesa.",
+                     "status": "sent"
+                  }
+               ]
+            },
+            {
+               "name": "Samuele",
+               "avatar": "./public/resources/img/avatar_3.jpg",
+               "visible": false,
+               "messages": [
+                  {
+                     "date": "28/03/2020 10: 10: 40",
+                     "text": "La Marianna va in campagna",
+                     "status": "received"
+                  },
+                  {
+                     "date": "28/03/2020 10: 20: 10",
+                     "text": "Sicuro di non aver sbagliato chat?",
+                     "status": "sent"
+                  },
+                  {
+                     "date": "28/03/2020 16: 15: 22",
+                     "text": "Ah scusa!",
+                     "status": "received"
+                  }
+               ]
+            },
+            {
+               "name": "Ilario",
+               "avatar": "./public/resources/img/avatar_4.jpg",
+               "visible": false,
+               "messages": [
+                  {
+                     "date": "10/01/2020 15: 30: 55",
+                     "text": "Lo sai che ha aperto una nuova pizzeria?",
+                     "status": "sent"
+                  },
+                  {
+                     "date": "10/01/2020 15: 50: 00",
+                     "text": "Si, ma preferirei andare al cinema",
+                     "status": "received"
+                  }
+               ]
             }
-        ]
+         ]
     },
     methods: {
         showConversation(index) {
